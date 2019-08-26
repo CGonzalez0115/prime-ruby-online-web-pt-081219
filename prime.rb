8f25b243a8f9098146c0c721ca25980c63459034
Christina Gonzalez <cmgamway@gmail.com>
2019-08-26 05:24:51 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,5 @@
 def prime?(n)
-  divisor = ("-50...50")
+  range = (-50..50)
   (2..n-1).none? { |division| n % division == 0 }
-  if n ==
-    return true
-  else
-    return false
-  end
+  puts "#{range}"
 end
